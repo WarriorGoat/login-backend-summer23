457a412c2089c1e5ffbea10ed4646d4d2f6b324c
Michael Spitz <michael.spitz@codeimmersives.com>
2023-06-06 13:56:22 -0400

    Inital commit

---
+++ routes/users/users.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+const usersController = require('./controller/usersController');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/login-test', function(req, res) {
+  console.log(req.body)
+  res.send({
+    username: req.body.username
+  });
+});
+
+router.post('/login', usersController.login)
+
+
+module.exports = router;
